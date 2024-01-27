e52055e21e5892413176643a0c023ccc14a6722c
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-27 18:18:38 -0500

    Add files via upload

---
+++ ng2-hangman-master/src/app/playgame.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { PlayGameService } from './playgame.service';
+
+describe('PlayGameService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PlayGameService]
+    });
+  });
+
+  it('should ...', inject([PlayGameService], (service: PlayGameService) => {
+    expect(service).toBeTruthy();
+  }));
+});
